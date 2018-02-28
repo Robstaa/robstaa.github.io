21ca4837a3e2b897c2ecc65a305fed94b2bc0806
Robin Zuschke <robin.l.zuschke@gmail.com>
2018-02-28 15:24:28 +0100

    Implemented lifetime supply calculator and updated readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Wiredelta excercise
+
+##Planning for layout of the website
+- Asking the user for their birthdate
+- Giving out the possible age(s)
+- Asking the user about their favorite snack
+- Asking the user: "How much of your SNACK do you eat in a day on average"
+- Giving out how much they would need until the max age
--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+// The age calculator
+
 function calculateAge(birthYear) {
   const date = new Date();
-  const currentYear = date.getFullYear();
+  const currentYear = date.getFullYear(); // Gets the current year as integer
 
-  if (birthYear === currentYear) {
+  if (birthYear == currentYear) {         // Using double equal signs so that the same number as string or integer would be equal
     return "You were just born or are about to be!";
   } else if (birthYear > currentYear) {
-    return "You are not born yet";
+    return "You are not born yet...";
   } else if (birthYear < currentYear) {
     const possibleAgeOne = currentYear - birthYear - 1;
     const possibleAgeTwo = currentYear - birthYear;
@@ -18,3 +20,25 @@ function calculateAge(birthYear) {
 calculateAge(1993);
 calculateAge("hello");
 calculateAge("2018");
+
+
+// The lifetyme supply calculator
+
+function calculateSupply(age, amountPerDay) {
+  const maxAge = 81  // Average life expectancy in Denmark
+
+  if (maxAge == age) {
+    return "I bet you need more, but I cannot tell you how much...";
+  } else if (maxAge < age) {
+    return "Oh wow, that snack must have really kept you going over all these years.";
+  } else if (maxAge > age) {
+    const totalSnackAmount = (maxAge - age) * amountPerDay * 365;
+    return `You will need ${Math.round(totalSnackAmount)} to last you until the ripe old age of ${maxAge}!`;
+  } else {
+    return "Your input must be viable numbers."
+  }
+};
+
+calculateSupply(60, 1.5)
+calculateSupply("hi", 20)
+calculateSupply("82", "12.2")
